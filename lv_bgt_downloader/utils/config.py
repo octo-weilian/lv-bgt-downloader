6068fc68aa81a00f094c3efcf4dffbdbfc13f4ec
william.live94@gmail.com <william.live94@gmail.com>
2022-11-07 21:25:03 +0100

    modified config get methods

--- lv_bgt_downloader/utils/config.py
+++ lv_bgt_downloader/utils/config.py
@@ -4,18 +4,15 @@ class appConfig:
     def __init__(self,config_file):
         self.config_file = config_file
         self.parser = self.read_config()
-        
-    def get(self,section,option):
-        return self.parser[section][option]
-    
+
     def read_config(self):
-        if Path(self.config_file).exists():
+        if Path(self.config_file).is_file() and Path(self.config_file).exists():
             parser = ConfigParser()
             parser.read(self.config_file)
             return parser
         else:
-            return None
-            
+            raise FileNotFoundError(f"{self.config_file} does not exists.")
+
     def save_config(self,section,values):
         options = [option for option,_ in self.parser.items(section)]
         for i in range(len(options)):
---
+++ main.py
@@ -0,0 +1,64 @@
+from lv_bgt_downloader.lv_bgt import LV_BGT
+from lv_bgt_downloader.pdok import BGT_API
+from lv_bgt_downloader.utils.config import appConfig
+
+#load app configfile 
+APP_CONFIG = appConfig("appConfig.ini").parser
+
+#project config
+OUTPUT_DIR = APP_CONFIG.get("project","output_dir")
+INPUT_AOI = APP_CONFIG.get("project","input_aoi")
+DROP_FEATURES = list(filter(None,APP_CONFIG.get("project","drop_features").split(";")))
+
+#processing config
+ADD_PBP = APP_CONFIG.getboolean("processing","add_pbp")
+MAKE_GML = APP_CONFIG.getboolean("processing","make_gml")
+MAKE_CAD = APP_CONFIG.getboolean("processing","make_cad")
+CLEANUP_CAD = APP_CONFIG.getboolean("processing","cleanup_cad")
+
+if __name__=="__main__":
+    lv_bgt = LV_BGT(INPUT_AOI,OUTPUT_DIR)
+    
+    #download stufgeo extract and build new stufgeo
+    all_features =  BGT_API.get_features().keys()
+    selected_features = sorted(list(set(all_features)-set(DROP_FEATURES)))
+    extract_xml = lv_bgt.download_stufgeo(selected_features)
+    stufgeo_xml = lv_bgt.build_stufgeo(extract_xml)
+
+    #add PBPs
+    if ADD_PBP:
+        pbp_xml = lv_bgt.download_stufgeo(["plaatsbepalingspunt"])
+        lv_bgt.add_stufgeo_pbp(stufgeo_xml,pbp_xml)
+    
+    #convert stufgeo XML to GML
+    if MAKE_GML:
+        lv_bgt.convert_stufgeo(stufgeo_xml,"GML")
+
+    #convert stufgeo XML to DXF
+    if MAKE_CAD:
+        lv_bgt.convert_stufgeo(stufgeo_xml,"DXF",CLEANUP_CAD)
+    
+
+
+
+   
+
+    
+    
+
+    
+    
+    
+
+
+
+
+  
+        
+
+
+
+
+
+        
+
